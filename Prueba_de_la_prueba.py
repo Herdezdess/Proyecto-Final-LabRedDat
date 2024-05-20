f9f1dadcd60ff72936d74e25d3cd6b399cafab84
Herdezdess <160191503+Herdezdess@users.noreply.github.com>
2024-05-19 20:22:34 -0600

    Update Prueba_de_la_prueba.py

--- Prueba_de_la_prueba.py
+++ Prueba_de_la_prueba.py
@@ -65,7 +65,7 @@ def load_data():
     return exploded_track_df #retorno a df para que ajá se pueda hacer varias veces y no se muera
 
 #como temos varias caracteristicas, separamos los generos de las demás caracteristicas 
-genero_names = ['Dance Pop', 'Electronic', 'Electropop', 'Hip Hop', 'Jazz', 'K-pop', 'Latin', 'Pop', 'Pop Rap', 'R&B', 'Rock']
+genre_names = ['Dance Pop', 'Electronic', 'Electropop', 'Hip Hop', 'Jazz', 'K-pop', 'Latin', 'Pop', 'Pop Rap', 'R&B', 'Rock']
 audio_feats = ["acousticness", "danceability", "energy", "instrumentalness", "valence", "tempo"]
 
 #le damos nombre a lo anterior
@@ -86,19 +86,21 @@ def n_neighbors_uri_audio(genre, start_year, end_year, test_feat):
     uris = genre_data.iloc[n_neighbors]["uri"].tolist()
     # y las caracteristicas
     audios = genre_data.iloc[n_neighbors][audio_feats].to_numpy()
-    return uris, audios
+    return uris, audios #devielve los url y las caracteristicas
 
-
+#titulo y demás
 st.markdown("<h1 style='text-align: center; color: #F74B66; text-shadow: 3px 3px #808080;'> Modelo de recomendación de canciones </h1>", unsafe_allow_html=True)
 
 st.markdown("A continuación se le pedirán ciertas características para hacer una predicción ")
 st.markdown("##")
 
+#hacemos que el usuario elija un genero
+#botones de genero, Dance pop por defenco
 st.markdown("***Elija el género***")
 genre = st.radio(
     "",
     genre_names, index=genre_names.index("Dance Pop"))
-
+#le pedimos al usuario que elija las caracteristicas, le mostramos sliders con datos por defecto
 with st.container():
     col1, col2,col3,col4 = st.columns((2,0.5,2,0.5))
     with col1:
@@ -128,48 +130,51 @@ with st.container():
             0.0, 244.0, 244.0)
 
 
+#mostramos 4 canciones
+tracks_per_page = 4
+test_feat = [acousticness, danceability, energy, instrumentalness, valence, tempo]#carateristicas
+uris, audios = n_neighbors_uri_audio(genre, start_year, end_year, test_feat)#guarda el erl y las caracteristicas de las canciones basadas en el resultado del entrenamiento
 
-
-
-
-tracks_per_page = 6
-test_feat = [acousticness, danceability, energy, instrumentalness, valence, tempo]
-uris, audios = n_neighbors_uri_audio(genre, start_year, end_year, test_feat)
-
+#una lista que almacene las canciones
 tracks = []
-for uri in uris:
-    track = """<iframe src="https://open.spotify.com/embed/track/{}" width="260" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>""".format(uri)
-    tracks.append(track)
+for uri in uris:#bucle
+    track = """<iframe src="https://open.spotify.com/embed/track/{}" width="260" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>""".format(uri) #hacemos los cuadros para las canciones
+    tracks.append(track) #lista de cadenas de cada pista
 
-if 'previous_inputs' not in st.session_state:
+#al principio de código incdicamos que se guarden los datos/cache. aquí recordamos los valores que teemos para que no haya problema entre las iteraciones y la aplicación no se muera
+if 'previous_inputs' not in st.session_state: 
     st.session_state['previous_inputs'] = [genre, start_year, end_year] + test_feat
 
+#creamos una lista que contengan los datos que queremos 
 current_inputs = [genre, start_year, end_year] + test_feat
+#verificamos que los valores actuales no sean iguales a los anteriores, si eso es verdadero entonces:
 if current_inputs != st.session_state['previous_inputs']:
     if 'start_track_i' in st.session_state:
         st.session_state['start_track_i'] = 0
-    st.session_state['previous_inputs'] = current_inputs
+    st.session_state['previous_inputs'] = current_inputs #actualizamos la lista de los datos que queremos
 
 if 'start_track_i' not in st.session_state:
     st.session_state['start_track_i'] = 0
 
+#dividimos la página
 with st.container():
     col1, col2, col3 = st.columns([2,1,2])
+    #si no queremos obtener los mismos resultados hacemos el procedimiento de nuevo
     if st.button("Recommend More Songs"):
         if st.session_state['start_track_i'] < len(tracks):
             st.session_state['start_track_i'] += tracks_per_page
-
     current_tracks = tracks[st.session_state['start_track_i']: st.session_state['start_track_i'] + tracks_per_page]
     current_audios = audios[st.session_state['start_track_i']: st.session_state['start_track_i'] + tracks_per_page]
+#mostramos las canciones
     if st.session_state['start_track_i'] < len(tracks):
         for i, (track, audio) in enumerate(zip(current_tracks, current_audios)):
-            if i%2==0:
-                with col1:
-                    components.html(
+            if i%2==0:#dos columnas
+                with col1:#indices pares
+                    components.html(#lo mostramos en streamlit
                         track,
                         height=400,
                     )
-                    with st.expander("See more details"):
+                    with st.expander("Características"):#mostramos las características
                         df = pd.DataFrame(dict(
                         r=audio[:5],
                         theta=audio_feats[:5]))
@@ -177,13 +182,13 @@ with st.container():
                         fig.update_layout(height=400, width=340)
                         st.plotly_chart(fig)
         
-            else:
+            else: #lo mismo pero para la otra columna
                 with col3:
                     components.html(
                         track,
                         height=400,
                     )
-                    with st.expander("See more details"):
+                    with st.expander("Características"):
                         df = pd.DataFrame(dict(
                             r=audio[:5],
                             theta=audio_feats[:5]))
@@ -191,5 +196,5 @@ with st.container():
                         fig.update_layout(height=400, width=340)
                         st.plotly_chart(fig)
 
-    else:
-        st.write("No songs left to recommend")
+    else:#en caso de que ninguna cancion se acerque a lo que el usuario pide
+        st.write("No hay canciones para recomendar :c")
